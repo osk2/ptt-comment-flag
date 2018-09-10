bfe1917787b43de0628afca999d388fabfe45eb2
osk2 <sc60714@gmail.com>
2018-09-10 20:13:32 +0800

    Adjust response format

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const geoip2 = require('geoip2');
 const ipValidation = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
 
 app.use(bodyParser.json());
+app.use(cors());
+app.use('/assets', express.static('resources/flags'));
 geoip2.init('./resources/GeoLite2-City.mmdb');
 
 const getIp = ip => {
@@ -16,11 +20,17 @@ const getIp = ip => {
       if (err) {
         return reject(err);
       }
-      console.log(result);
       return resolve(result);
     });
   });
 }
+
+app.options('/*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.send(200);
+});
 
 app.post('/ip', async (req, res) => {
   const ipList = req.body.ip;
@@ -31,7 +41,11 @@ app.post('/ip', async (req, res) => {
     }
   
     const resolvedIPList = await Promise.all(ipList.map(ip => getIp(ip)));
-    const flagList = resolvedIPList.map(ip => `/resources/flags/${ip.country.toLowerCase()}.png`);
+    const flagList = resolvedIPList.map(ip => {
+      const imagePath = `assets/${ip.country.toLowerCase()}.png`;
+      const locationName = ip.city ? `${ip.city}, ${ip.country}` : ip.coutry;
+      return { imagePath, locationName };
+    });
     res.json(flagList);
   } catch (ex) {
     console.error(ex);
@@ -39,4 +53,4 @@ app.post('/ip', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(9977, () => console.log('App listening on port 9977!'));
