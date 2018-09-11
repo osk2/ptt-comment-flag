93a9d8663861151b9849eee29a9ebe7fef773f12
FlandreDaisuki <vbnm123c@gmail.com>
2018-09-11 18:31:05 +0800

    Support userscript

---
+++ userscript/build.js
@@ -0,0 +1,25 @@
+// npm run build:userscript
+
+const rollup = require('rollup');
+const metablock = require('rollup-plugin-userscript-metablock');
+const pkg = require('../package.json');
+
+main();
+
+async function main() {
+  const bundle = await rollup.rollup({
+    input: '../chrome-extension/js/content_script.js',
+    plugins: [
+      metablock({
+        file: './metablock.json',
+        version: pkg.version,
+      }),
+    ],
+  });
+
+  await bundle.write({
+    file: 'ptt-comment-flag.user.js',
+    format: 'iife',
+  });
+}
+
