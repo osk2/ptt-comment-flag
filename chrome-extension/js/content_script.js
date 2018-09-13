284b5a7fa8fdafce4205349490a6284c396b88ba
osk2 <sc60714@gmail.com>
2018-09-13 16:57:26 +0800

    Minor code improvement

--- chrome-extension/js/content_script.js
+++ chrome-extension/js/content_script.js
@@ -1,13 +1,12 @@
-+(async () => {
+(async () => {
   try {
     const HOST = 'https://osk2.me:9977';
-    const comment = document.querySelectorAll('.push-ipdatetime');
-    const commentArray = Array.from(comment);
-    const ipList = commentArray.map(node => node.textContent.trim().split(' ')[0]);
+    const comments = document.querySelectorAll('.push-ipdatetime');
+    const ipList = [].map.call(comments, node => node.textContent.trim().split(' ')[0]);
     const flagsResponse = await axios.post(`${HOST}/ip`, { ip: ipList });
     const flags = flagsResponse.data;
   
-    commentArray.forEach((comment, index) => {
+    [].forEach.call(comments, (comment, index) => {
       const datetime = comment.textContent.trim().split(' ').splice(1, 2).join(' ');
       const imagePath = flags[index].imagePath ? `${HOST}/${flags[index].imagePath}` : null;
       const imageTitile = flags[index].locationName;
