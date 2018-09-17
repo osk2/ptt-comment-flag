9ed1109f30b2b9dfaa754840f2fa895d561e290f
osk2 <sc60714@gmail.com>
2018-09-17 13:43:10 +0800

    Release v1.7 for userscript

--- userscript/ptt-comment-flag.user.js
+++ userscript/ptt-comment-flag.user.js
@@ -2,7 +2,7 @@
 // @name        八卦插IP
 // @namespace   https://github.com/osk2/ptt-comment-flag/
 // @description 開門！查IP
-// @version     1.6
+// @version     1.7
 // @author      osk2
 // @match       https://www.ptt.cc/bbs/Gossiping/M*
 // @match       https://www.ptt.cc/bbs/gossiping/M*
@@ -15,26 +15,55 @@
 (async () => {
   try {
     const HOST = 'https://osk2.me:9977';
+    const ipValidation = /((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/;
     const comments = document.querySelectorAll('.push-ipdatetime');
-    const ipList = [].map.call(comments, node => node.textContent.trim().split(' ')[0]);
-    const flagsResponse = await axios.post(`${HOST}/ip`, { ip: ipList });
-    const flags = flagsResponse.data;
+    const f2Nodes = document.querySelectorAll('.f2');
+    const authorComment = [].filter.call(f2Nodes, n => n.innerHTML.match(ipValidation));
+    const authorIpList = authorComment.map(c => c.innerHTML.match(ipValidation)[0]);
+    const commentIpList = [].map.call(comments, n => n.textContent.trim().split(' ')[0]);
+    const authorFlagsResponse = await axios.post(`${HOST}/ip`, { ip: authorIpList });
+    const authorFlags = authorFlagsResponse.data;
+
+    const generateImageHTML = (ip, flag) => {
+      const imagePath = flag.imagePath ? `${HOST}/${flag.imagePath}` : null;
+      const imageTitile = `${flag.locationName || 'N/A'}<br>
+        <a href='https://www.google.com/search?q=${ip}' target='_blank'>${ip}</a>`;
+      
+      if (!imagePath) {
+        return;
+      }
+      return `<img data-flag src="${imagePath}" title="${imageTitile}">`;
+    };
+
+    authorComment.forEach((comment, index) => {
+      const ip = comment.innerHTML.match(ipValidation)[0];
+      const imageHTML = generateImageHTML(ip, authorFlags[index]);
+
+      if (!imageHTML) {
+        return;
+      }
+      comment.innerHTML = comment.innerHTML.replace(ipValidation, `${imageHTML} ${ip}`);
+    });
+
+    if (commentIpList.lenth === 0) {
+      return;
+    }
+
+    const commentFlagsResponse = await axios.post(`${HOST}/ip`, { ip: commentIpList });
+    const flags = commentFlagsResponse.data;
   
     [].forEach.call(comments, (comment, index) => {
       const commentInfo = comment.textContent.trim().split(' ');
       const commentIp = commentInfo[0];
-      const commentId = comment.previousSibling.previousSibling.innerHTML;
       const commentDatetime = commentInfo.splice(1, 2).join(' ');
-      const imagePath = flags[index].imagePath ? `${HOST}/${flags[index].imagePath}` : null;
-      const imageTitile = `${flags[index].locationName || 'N/A'}<br>
-        <a href='https://www.google.com/search?q=${commentId}+${commentIp}' target='_blank'>${commentIp}</a>`;
-      const imageHTML = `<img data-flag src="${imagePath}" title="${imageTitile}">`;
+      const imageHTML = generateImageHTML(commentIp, flags[index]);
   
-      if (!imagePath) {
+      if (!imageHTML) {
         return;
       }
       comment.innerHTML = `${imageHTML} ${commentDatetime}`;
     });
+
     tippy('[data-flag]', {
       arrow: true,
       size: 'large',
